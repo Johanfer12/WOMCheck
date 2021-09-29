2853a315c809aae0df1d87108806bce53d42a314
Johan <johanfer_12@hotmail.com>
2021-09-29 07:36:15 -0500

    Update WOM_Check.py

--- WOM_Check.py
+++ WOM_Check.py
@@ -1,52 +1,53 @@
 from selenium import webdriver
-import webbrowser
 from datetime import datetime
-import time
-import os
+from selenium.webdriver.chrome.options import Options
 
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome(executable_path=r'C:\chromedriver_win32\chromedriver.exe', options=options)
 
 #Load login
 login = open('login.txt').read().splitlines()
 
-driver = webdriver.Chrome()
-driver.set_window_size(1360, 968)
-
+#Get webpage
 driver.get('https://www.wom.co/customer/consumption/')
-time.sleep(1)
 
-#Click
+#Click options
+
 driver.find_element_by_xpath('/html/body/div[2]/div/div/button').click()
 
 x = driver.find_element_by_xpath("/html/body/div[3]/main/div[3]/div/div[2]/div[2]/div[1]/form/div[1]/input[2]")
-
-x.clear()
-x.send_keys(login[0])
-time.sleep(1)
 x.clear()
 x.send_keys(login[0])
 
-time.sleep(1)
+driver.find_element_by_xpath('/html/body/div[3]/main/div[3]/div/div[2]/div[2]/div[1]/form').click()
+
 driver.find_element_by_xpath('/html/body/div[3]/main/div[3]/div/div[2]/div[2]/div[1]/form/div[2]/div/input').send_keys(login[1])
-time.sleep(1)
+
 driver.find_element_by_xpath('/html/body/div[3]/main/div[3]/div/div[2]/div[2]/div[1]/form/div[4]/div[1]/div[2]/button').click()
 
 driver.find_element_by_xpath('/html/body/div[3]/main/div[2]/div[2]/div/div[2]/ul/li[2]/a').click()
 
+#Usage calculations
 
 use = driver.find_element_by_xpath("/html/body/div[3]/main/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/p/span").text
-
 limitdate = driver.find_element_by_xpath("/html/body/div[3]/main/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[2]/div[2]/p[2]").text
 
-# d2 = limitdate
-# 
-# d1 = datetime.strptime(d1, "%d-%m-%Y")
-# d2 = datetime.strptime(d2, "%d-%m-%Y")
-# 
-# delta = d2 - d1
+today = datetime.today().strftime('%d-%m-%Y')
+start = datetime.strptime(today, "%d-%m-%Y")
+end = datetime.strptime(limitdate, "%d/%m/%Y")
 
+diff = end.date() - start.date()
+use1 = use.replace(" GIGAS","")
 
+left = float(120 - float(use1))
+gbxday = float(left/(float(diff.days)))
+answer = str(round(gbxday, 2))
 
-print(use)
-print(limitdate)
-# print(delta.days)
+print("Quedan: " + str(left) + " GB")
+print("Para usar en " + str(diff.days) + " días.")
+print("GB por día: " + answer)
 
+#Driver close
+driver.close()
